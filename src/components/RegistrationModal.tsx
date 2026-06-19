67124d7105eb19b9b1a0178f118fab841896d82b
M4NithinReddy <mnithinreddy0403@gmail.com>
2026-06-19 08:28:23 +0530

    commit4edwf

--- src/components/RegistrationModal.tsx
+++ src/components/RegistrationModal.tsx
@@ -97,7 +97,7 @@ const RegistrationModal: React.FC<RegistrationModalProps> = ({
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
-                registrationId: orderData.registrationId,
+                registrationData: orderData.registrationData,
                 razorpay_order_id: response.razorpay_order_id,
                 razorpay_payment_id: response.razorpay_payment_id,
                 razorpay_signature: response.razorpay_signature
